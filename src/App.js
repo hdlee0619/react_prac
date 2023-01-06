ea4f87aadd14000a7b6d4d80fe7995a8507792fe
이현동 (Hyeondong Lee) <96946263+hdlee0619@users.noreply.github.com>
2023-01-06 10:39:00 +0900

    To Do list
    간단한 To Do list를 만들어 봄

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [toDo, setToDo] = useState("");
+   const [toDos, setToDos] = useState([]);
+   const onChange = (event) => setToDo(event.target.value);
+   const onSubmit = (event) => {
+      event.preventDefault();
+      if (toDo === "") {
+         return;
+      }
+      setToDo(""); // state 함수에 값을 보낼 수 있음
+      setToDos((currentArray) => [toDo, ...currentArray]); // state 함수에 함수를 보낼 수 있음
+   };
+
+   return (
+      <div>
+         <form onSubmit={onSubmit}>
+            <h1>My To Dos : count ({toDos.length})</h1>
+            <input
+               value={toDo}
+               onChange={onChange}
+               type="text"
+               placeholder="Write your to do.."
+            ></input>
+            <button>Add To Do</button>
+         </form>
+         <hr />
+         <ul>
+            {toDos.map((item, index) => (
+               <li key={index}>{item}</li>
+            ))}
+         </ul>
+      </div>
+   );
 }
 
 export default App;
